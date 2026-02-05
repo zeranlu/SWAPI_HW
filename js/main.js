e70c9ce775346051eeaeee1eb3179da8c2fa8711
Zeran Lu <zeranlu0326@gmail.com>
2026-02-05 14:05:39 -0500

    Update main.js
    In this commit, character ID were created as variables

--- js/main.js
+++ js/main.js
@@ -13,25 +13,47 @@ const characterBox = document.querySelector("#character-box");
 
 const baseURL = `https://swapi.info/api/`
 const charURL = `https://swapi.info/api/people/`;
-const filmURL = `https://swapi.info/api/films/2`;
+const filmURL = `https://swapi.info/api/films/`;
+
+const vaderID = '3';
+const tarkinID = '11';
+const jabbaID = '15';
+const palpatineID = '19';
+const bobaID = '20';
+const nuteID = '31';
+const maulID = '42';
+const dookuID = '65';
+const jangoID = '67';
+const grievousID = '77';
+
     
     function getCharacters() {
         fetch(`${charURL}`)
         .then((res) => res.json())
         .then(characters => {
+            
+            const ul =  document.createElement("ul");
+
+            console.log(characters[77].name);
 
             characters.forEach(character => {
-                console.log(character.name);
 
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.textContent = character.name;
+                li.appendChild(a);
+                ul.appendChild(li);
+                
                 // randomize the number that is picked
                 // figure out the length of the array then pick a number within that range
-                console.log(character.films);
+                // console.log(character.films);
 
                 // create a ul
                 // create a li
                 // create an a
                 // add a data attribute to the anchor tag that contains a film 
             })
+            characterBox.appendChild(ul);
         })
         .then(() => {
             //attach an event listener to each link, calls a new function that makes the second AJAX call
@@ -49,9 +71,9 @@ const filmURL = `https://swapi.info/api/films/2`;
         .then((res) => res.json())
         .then((movie) => {
 
-            console.log(`img.src="images/poster-${movie.episode_id}.jpg"`);
-            console.log(movie.title);
-            console.log(movie.opening_crawl);
+            // console.log(`img.src="images/poster-${movie.episode_id}.jpg"`);
+            // console.log(movie.title);
+            // console.log(movie.opening_crawl);
         })
         .catch((error) => {
             console.error(error)
